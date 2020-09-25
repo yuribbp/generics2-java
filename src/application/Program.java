f7722d2903532ba186202f601cece7d47eb074cc
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 11:47:28 -0300

    Simple Integer test with Generics

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,13 +6,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import entities.Product;
 import services.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<Integer> list = new ArrayList<>();
+		List<Product> list = new ArrayList<>();
 
 		String path = "C:\\temp\\in.txt";
 
@@ -20,12 +21,13 @@ public class Program {
 
 			String line = br.readLine();
 			while (line != null) {
-				list.add(Integer.parseInt(line));
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
 				line = br.readLine();
 			}
 			
-			Integer x = CalculationService.max(list);
-			System.out.println("Max:");
+			Product x = CalculationService.max(list);
+			System.out.println("Most expensive:");
 			System.out.println(x);
 
 		} catch (IOException e) {
